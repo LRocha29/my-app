c071580af9e2afd23ef393f858f04df6063ae74c
LRocha29 <luisrocha1929.lapr@gmail.com>
2022-08-08 09:20:34 -0500

    Nombre del proyecto: My app, Descripcion: Version final del proyecto my app

---
+++ src/main/java/com/luis/myapp/entity/TestEntity.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.luis.myapp.entity;
+
+/**
+ * @author Luis
+ *
+ */
+public class TestEntity {
+	/*
+	*Propiedad de prueba para muestra de compilacion del archivo testntity
+	*/
+private String nombre;
+
+/**
+ * @return the nombre
+ */
+public String getNombre() {
+	return nombre;
+}
+
+/**
+ * @param nombre the nombre to set
+ */
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+}
